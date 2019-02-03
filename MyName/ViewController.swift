00d086667c2165312256b733f9eba22ab0e9a5f7
Efren Duran Gomez <efrendurangomez@efrens-macbook-air.local>
2019-02-03 13:27:12 -0800

    Initial Commit

---
+++ MyName/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyName
+//
+//  Created by Efren Duran Gomez on 2019-02-03.
+//  Copyright © 2019 DuranTec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
